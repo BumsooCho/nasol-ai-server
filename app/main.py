b30d6e1914659b3584876e2f22fe8fc19290ed93
[gyparkdev] <[gyparkdev@gmail.com]>
2025-11-23 19:19:24 +0900

    feat: [나큰솔] 불특정 다수 유저는 pdf 형식의 지출 자료를 전송받아 Agent를 통해 분석한 뒤 Redis 서버에 업로드할 수 있다 [NSB-11]

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 from account.adapter.input.web.account_router import account_router
 from config.database.session import Base, engine
+from documents.adapter.input.web.document_router import documents_router
 from sosial_oauth.adapter.input.web.google_oauth2_router import authentication_router
 
 load_dotenv()
@@ -27,7 +28,7 @@ app.add_middleware(
 
 app.include_router(account_router, prefix="/account")
 app.include_router(authentication_router, prefix="/authentication")
-
+app.include_router(documents_router, prefix="/documents")
 # 앱 실행
 if __name__ == "__main__":
     import uvicorn
